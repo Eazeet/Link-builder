cdadb029bf0d3cadcb5bc9070b3e44ffe7670b40
Binaryy <ibolarinwa606@gmail.com>
2024-07-09 23:02:47 +0100

    adjusted snippet length

--- def_chat2.py
+++ def_chat2.py
@@ -76,7 +76,7 @@ def find_keyword_snippets(text, keyword, snippet_length=17):
     return "\n\n\n".join(snippets)
 
 
-def process_results(results, keyword, snippet_length=10):
+def process_results(results, keyword, snippet_length=17):
     processed_results = []
     
     for result in results:
