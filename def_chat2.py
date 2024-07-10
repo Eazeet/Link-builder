8375456d0c6411a6056a08d03fc4e339847cb059
Binaryy <ibolarinwa606@gmail.com>
2024-07-10 16:11:13 +0100

    adjusted for empty snippets

--- def_chat2.py
+++ def_chat2.py
@@ -89,7 +89,8 @@ def find_keyword_snippets(text, keyword, snippet_length=5):
             #     found_snippets.add(highlighted_snippet)
     
             return highlighted_snippet
-    return words[:5]
+    first_n_words = words[:5]    
+    return ' '.join(first_n_words)
 
 
 def process_results(results, keyword, snippet_length=5):
